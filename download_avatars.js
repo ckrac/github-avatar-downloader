ded8bae7f5fbb6e9597addb0e74cfdb6860f7320
vagrant <kennethracadio@hotmail.com>
2018-01-23 01:46:01 +0000

    Step 10: Make them required arguments

--- download_avatars.js
+++ download_avatars.js
@@ -2,31 +2,38 @@ var request = require('request');
 var getKey = require('./secrets.js');
 var fs = require('fs');
 var owner = process.argv[2]
-var repo = process.argv[3]
-// console.log(`owner: ${owner} repo: ${repo}`);
+var name = process.argv[3]
+console.log(`owner: ${owner} repo: ${name}`);
 
 
 // console.log('Welcome to the GitHub Avatar Downloader!');
 // console.log(getKey.GITHUB_TOKEN);
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      Authorization: 'token ' +  getKey.GITHUB_TOKEN
-    }
-  };
+// if statement to check if user have the required arguments on node
+  if(repoOwner && repoName) {
 
-  request(options, function(err, res, body) {
-    cb(err, body);
-  });
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        Authorization: 'token ' +  getKey.GITHUB_TOKEN
+      }
+    };
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+
+  } else {
+    console.log("Error: Please add your repoOwner and repoName to node.");
+  }
+
 }
 
+
+// function to use as call back to pipe recieved url to a folder
 function downloadImageByURL(url, filePath) {
-  // ...
 
   request.get(url)
     .on('error', function (err) {
@@ -39,7 +46,7 @@ function downloadImageByURL(url, filePath) {
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(owner, name, function(err, result) {
   console.log("Errors:", err);
   var data = JSON.parse(result);
   // console.log(result);
